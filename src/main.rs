d94957f5d1dd41a7eeffafc287edc7cf832eadd7
Richard Neumann <mail@richard-neumann.de>
2024-09-29 16:06:46 +0200

    Remove intermediate library

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,21 @@
-//! CLI tool to programmatically update offers on wg-gesucht.de
+//! CLI tool to programmatically update offers on wg-gesucht.de.
+
+pub use args::Args;
+pub use error::{Error, FailedUpdates};
+pub use session::Session;
+pub use settings::Settings;
+
+mod args;
+mod auth_data;
+mod config_file;
+mod error;
+mod functions;
+mod login_data;
+mod patch_data;
+mod session;
+mod settings;
 
 use clap::Parser;
-use wg_gesucht_updater::{Args, Settings};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
